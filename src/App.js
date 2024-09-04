f4281197cf10e6a0215db521a8d74127b6531745
Arvin <arvincris@gmail.com>
2024-09-04 16:05:17 +0800

    Building the Static App: List of Friends

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Groot",
+    image: "https://th.bing.com/th/id/OIP.l6ExHkrN4SE-v2Ap8PSMUQHaHa?rs=1&pid=ImgDetMain",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Gamora",
+    image: "https://th.bing.com/th/id/OIP.4fZR_ioMxGXVmr1bNYVg0QHaGc?rs=1&pid=ImgDetMain",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Quill",
+    image: "https://th.bing.com/th/id/OIP.V4qb2cLkFd64Fs6GEl8VWgHaHa?rs=1&pid=ImgDetMain",
+    balance: 0,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList/>
+      </div>
     </div>
   );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id}></Friend>
+      ))}
+    </ul>
+  )
+}
+
+function Friend({friend}) {
+  return <li>
+    <img src={friend.image} alt={friend.name}/>
+    <h3>{friend.name}</h3>
+    {friend.balance < 0 && <p className="red">You owe {friend.name} {Math.abs(friend.balance)} VND</p>}
+    {friend.balance > 0 && <p className="green">{friend.name} owes you {Math.abs(friend.balance)} VND</p>}
+    {friend.balance === 0 && <p>You and {friend.name} are even.</p>}
+    <button className="button">Select</button>
+  </li>
 }
 
 export default App;
